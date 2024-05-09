e9e768f58db7dc668288b1713336a25531c0fb3c
prince1096 <princeraj10121996@gmail.com>
2024-05-09 09:54:26 +0530

    add pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 // import React from 'react'
 
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUP from "./pages/SignUP";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUP />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 };
 
 export default App;
