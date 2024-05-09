0df164105146efcf7fbab7f336f6a9b1c04eba00
prince1096 <princeraj10121996@gmail.com>
2024-05-09 11:21:30 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 };
 
