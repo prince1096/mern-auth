d03e23cbc8048a433d3e90386e7d37fdffdd1acb
prince1096 <princeraj10121996@gmail.com>
2024-05-09 09:57:27 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
-import SignUP from "./pages/SignUP";
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUP />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   );
--- client/src/pages/SignUP.jsx
+++ client/src/pages/SignUP.jsx
@@ -1,5 +1,3 @@
-// import React from 'react'
-
 const SignUP = () => {
   return <div>SignUP</div>;
 };
