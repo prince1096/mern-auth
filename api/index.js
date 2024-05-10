a2ce4645f50dcb1cc094389a1decd3b146c6a15f
prince1096 <princeraj10121996@gmail.com>
2024-05-10 16:37:46 +0530

    connecttodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGO);
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Mongodb");
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("All Good");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
