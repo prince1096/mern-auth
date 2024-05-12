a2160a1198d877f6e828aae8199086a1b0d16c61
prince1096 <princeraj10121996@gmail.com>
2024-05-12 13:00:39 +0530

    addtoken

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,10 +38,13 @@ export const signin = async (req, res, next) => {
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(200)
-      .json(validUser);
+      .json(rest);
   } catch (error) {
     next(error);
   }
